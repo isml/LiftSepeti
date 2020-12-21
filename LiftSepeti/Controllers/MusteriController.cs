2ca943ccff3431f6d54ff71e6dd84106694d9d92
ismail karakaş <43730835+isml@users.noreply.github.com>
2020-12-21 17:02:25 +0300

    ilk

---
+++ LiftSepeti/Controllers/MusteriController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LiftSepeti.Models.Entity;
+
+
+namespace LiftSepeti.Controllers
+{
+    public class MusteriController : Controller
+    {
+        // GET: Musteri
+        LiftSepetiEntities1 db = new LiftSepetiEntities1();
+        public ActionResult Index()
+        {
+            var degerler = db.musteriTable.ToList();
+            return View(degerler);
+        }
+    }
+}
